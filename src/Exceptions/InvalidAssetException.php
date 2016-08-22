1bb67e9d0e6e69c70b62cae41c3ba8bc9af000eb
simius <simiust@gmail.com>
2016-08-22 17:37:31 +0200

    Missing file.

---
+++ src/Exceptions/InvalidAssetException.php
@@ -0,0 +1,12 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  InvalidAssetException.php - Part of the AssetHandler project.
+
+  File created by Johannes Tegnér at 2016-08-22 - kl 15:44
+  © - 2016
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jite\AssetHandler\Exceptions;
+
+use Exception;
+
+class InvalidAssetException extends Exception { }
