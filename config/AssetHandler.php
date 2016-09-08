1baa673689e45df970ff39458da027bb213273f4
simius <simiust@gmail.com>
2016-09-08 16:44:37 +0200

    Created a config file.

---
+++ config/AssetHandler.php
@@ -0,0 +1,38 @@
+<?php
+return [
+
+    /*
+    |----------------------------------------------------------------
+    | Asset containers.
+    |----------------------------------------------------------------
+    |
+    | Asset containers to be loaded.
+    |
+    */
+    "containers" => array(
+        "images" => [
+            "print_pattern" => '<img src="{{PATH}}">',
+            "file_regex"    => "/\\.(jpg|jpeg|tiff|gif|png|bmp|ico)$/i",
+            "path"          => "/public/assets/images",
+            "url"           => "/assets/images",
+            "versioned"     => false
+        ],
+        "scripts" => [
+            "print_pattern" => '<script src="{{PATH}}" type="application/javascript"></script>',
+            "file_regex"    => "/\\.js$/i",
+            "path"          => "/public/assets/scripts",
+            "url"           => "/assets/scripts",
+            "versioned"     => true
+        ],
+        "style sheets" => [
+            "print_pattern" => '<link rel="stylesheet" type="text/css" href="{{PATH}}" title="{{NAME}}">',
+            "file_regex"    => "/\\.css$/i",
+            "path"          => "/public/assets/styles",
+            "url"           => "/assets/styles",
+            "versioned"     => true
+        ],
+        //
+        // Add your own custom containers here.
+        //
+    )
+];
