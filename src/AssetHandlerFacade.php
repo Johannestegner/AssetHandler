e46f400634b3a0995de8ac5fde33c29609e29997
simius <simiust@gmail.com>
2016-09-11 19:27:45 +0200

    Created Facade class.

---
+++ src/AssetHandlerFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  AssetHandlerFacade.php - Part of the AssetHandler project.
+
+  File created by Johannes Tegnér at 2016-09-11 - kl 19:21
+  © - 2016
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jite\AssetHandler;
+
+use Illuminate\Support\Facades\Facade;
+
+class AssetHandlerFacade extends Facade {
+
+    /**
+     * @inheritDoc
+     */
+    protected static function getFacadeAccessor() {
+        return 'AssetHandler';
+    }
+}
